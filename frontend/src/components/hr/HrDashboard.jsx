0b57702b13158b35741c98be575895511a0ff73b
Ashutosh <ashutoshnautiyal94@gmail.com>
2023-08-08 01:21:25 +0530

    trying with the job descriptions form

--- frontend/src/components/hr/HrDashboard.jsx
+++ frontend/src/components/hr/HrDashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import { Link, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import { IoDocumentOutline } from 'react-icons/io5'
+import { IoDocumentOutline } from "react-icons/io5";
 import { FaUserGraduate } from "react-icons/fa";
 import PostJD from "./PostJD";
 
@@ -47,7 +47,6 @@ function HrDashboard() {
     const data = await response.json();
     setData(data); // Update state with the fetched data
   };
-
 
   const fetchJobData = async (hr_id) => {
     const response = await fetch("/api/posted_jd", {
@@ -131,18 +130,28 @@ function HrDashboard() {
   };
 
   return (
-    <div className='flex '>
-      <Sidebar />
-      <div className='flex flex-col w-full'>
+    <div className="flex dark:bg-neutral-900 ">
+      <div className="fixed left-0">
+        <Sidebar />
+      </div>
+      <div className="flex flex-col w-full md:ml-[17rem]">
         <Navbar />
-        <div className="text-xl bg-white/90 dark:bg-neutral-900 duration-300 min-h-screen  font-semibold p-4">
-          <h1 className="text-3xl font-semibold dark:text-white/90 px-4 ">Dashboard</h1>
-          <h2 className="text-4xl font-semibold px-4 dark:text-white/80 mt-2">Welcome {data.name}</h2>
-          <div className='grid grid-cols-1 lg:grid-cols-4 gap-4 px-4 mt-12'>
+        <div className=" text-xl bg-white/90 dark:bg-neutral-900 duration-300 min-h-screen  font-semibold p-4">
+          <h1 className="text-3xl font-semibold dark:text-white/90 px-4 ">
+            Dashboard
+          </h1>
+          <h2 className="text-4xl font-semibold px-4 dark:text-white/80 mt-2">
+            Welcome {data.name}
+          </h2>
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 px-4 mt-12">
             <div className="bg-zinc-100 shadow-lg hover:shadow-sky-600 duration-300 dark:bg-neutral-800 rounded-xl p-4 flex flex-row justify-between">
               <div className="">
-                <h2 className="text-xl font-normal dark:text-neutral-500 ">Total JD Posted</h2>
-                <h1 className="text-3xl font-semibold dark:text-white">{jdCount}</h1>
+                <h2 className="text-xl font-normal dark:text-neutral-500 ">
+                  Total JD Posted
+                </h2>
+                <h1 className="text-3xl font-semibold dark:text-white">
+                  {jdCount}
+                </h1>
               </div>
               <div>
                 <IoDocumentOutline className="text-6xl text-gray-400 dark:text-neutral-300" />
@@ -150,7 +159,9 @@ function HrDashboard() {
             </div>
             <div className="bg-zinc-100 shadow-lg hover:shadow-sky-600 duration-300 dark:bg-neutral-800 rounded-xl p-4 flex flex-row justify-between">
               <div className="">
-                <h2 className="text-xl font-normal dark:text-neutral-500 ">Candidates Applied</h2>
+                <h2 className="text-xl font-normal dark:text-neutral-500 ">
+                  Candidates Applied
+                </h2>
                 <h1 className="text-3xl font-semibold dark:text-white">{4}</h1>
               </div>
               <div>
@@ -160,42 +171,59 @@ function HrDashboard() {
           </div>
 
           <div className="mt-12">
-            <h1 className="text-3xl font-semibold dark:text-white/80 px-4">Posted Job Description</h1>
+            <h1 className="text-3xl font-semibold dark:text-white/80 px-4">
+              Posted Job Description
+            </h1>
             <div className="mt-4 px-16">
-              {
-                jobData.map((job) => (
-                  <div className="bg-zinc-100 shadow-lg hover:shadow-sky-600 duration-300 dark:bg-neutral-800 rounded-xl p-4 flex flex-col justify-between mb-4" key={job.jd_id}>
+              {jobData.map((job) => (
+                <div
+                  className="bg-zinc-100 shadow-lg hover:shadow-sky-600 duration-300 dark:bg-neutral-800 rounded-xl p-4 flex flex-col justify-between mb-4"
+                  key={job.jd_id}
+                >
+                  <Link to={`/hr/home/${job.jd_id}`}>
                     <div className="flex flex-row justify-between">
                       <div className="flex flex-col">
                         <div className="flex flex-col">
-                          <h2 className="text-xl font-normal dark:text-neutral-500 ">Role</h2>
-                          <h1 className="text-3xl font-semibold dark:text-white">{job.title}</h1>
+                          <h2 className="text-xl font-normal dark:text-neutral-500 ">
+                            Role
+                          </h2>
+                          <h1 className="text-3xl font-semibold dark:text-white">
+                            {job.title}
+                          </h1>
                         </div>
                         <div className="flex flex-col">
-                          <h2 className="text-xl font-normal dark:text-neutral-500 ">Description</h2>
-                          <h1 className="text-3xl font-semibold dark:text-white">{job.description}</h1>
+                          <h2 className="text-xl font-normal dark:text-neutral-500 ">
+                            Description
+                          </h2>
+                          <h1 className="text-3xl font-semibold dark:text-white">
+                            {job.description}
+                          </h1>
                         </div>
                       </div>
                       <div className="flex flex-col">
                         <div className="flex flex-col">
-                          <h2 className="text-xl font-normal dark:text-neutral-500 ">Applied</h2>
-                          <h1 className="text-3xl font-semibold text-center dark:text-white">{job.applied}</h1>
+                          <h2 className="text-xl font-normal dark:text-neutral-500 ">
+                            Applied
+                          </h2>
+                          <h1 className="text-3xl font-semibold text-center dark:text-white">
+                            {job.applied}
+                          </h1>
                         </div>
                         <div className="flex flex-col">
-                          <button className="bg-sky-600 hover:bg-sky-700 duration-300 text-white rounded-lg px-4 py-2 mt-4" onClick={() => handleToggle(job.jd_id)}>
+                          <button
+                            className="bg-sky-600 hover:bg-sky-700 duration-300 text-white rounded-lg px-4 py-2 mt-4"
+                            onClick={() => handleToggle(job.jd_id)}
+                          >
                             Edit
                           </button>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))
-
-              }
-
+                  </Link>
+                </div>
+              ))}
             </div>
           </div>
-
         </div>
       </div>
     </div>
